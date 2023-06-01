991a6cfb1f065cb67047f8198a4f44ac4dad3fa3
Gustavo Barros <gustavo.barros@vibetecnologia.com>
2023-06-01 16:51:06 -0300

    Aula de C# - Operadores aritiméticos

--- PrimeiraAula/PrimeiraAula/Program.cs
+++ PrimeiraAula/PrimeiraAula/Program.cs
@@ -11,22 +11,102 @@ namespace Course
         {
 
 
-            int a = 10;
-            Console.WriteLine(a);
+         
 
-            a += 2;
-            Console.WriteLine(a);
-
-            a *= 3;
-            Console.WriteLine(a);
-
-            Console.ReadLine();
+        Console.ReadLine();
 
         }
 
     }
 
 }
+
+
+
+
+
+
+//using System;
+//using System.Globalization;
+
+//namespace Course
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+
+//            int n1 = 3 + 4 * 2;
+//            int n2 = (3 + 4) * 2;
+//            int n3 = 17 % 3;
+//            double n4 = (double) 10 / 8;
+
+//            double a = 1.0, b = -3.0, c = -4.0;
+
+//            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+
+//            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+
+//            Console.WriteLine(n1);
+//            Console.WriteLine(n2);
+//            Console.WriteLine(n3);
+//            Console.WriteLine(n4);
+//            Console.WriteLine(x1);
+
+//            Console.ReadLine();
+
+//        }
+
+//    }
+
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using System;
+//using System.Globalization;
+
+//namespace Course
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+
+//            int a = 10;
+//            Console.WriteLine(a);
+
+//            a += 2;
+//            Console.WriteLine(a);
+
+//            a *= 3;
+//            Console.WriteLine(a);
+
+//            Console.ReadLine();
+
+//        }
+
+//    }
+
+//}
 
 
 
