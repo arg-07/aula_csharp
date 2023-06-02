923f4484089270421ba13c7d195c81025cb64992
Gustavo Barros <gustavo.barros@vibetecnologia.com>
2023-06-01 22:06:57 -0300

    aula C# com .NET - entrada de dados

--- PrimeiraAula/PrimeiraAula/Program.cs
+++ PrimeiraAula/PrimeiraAula/Program.cs
@@ -9,11 +9,25 @@ namespace Course
     {
         static void Main(string[] args)
         {
+            
+        int n1 = int.Parse(Console.ReadLine());
+        char ch = char.Parse(Console.ReadLine());
+        double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string[] nome = vet[0];
+            string[] sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
 
 
-         
 
-        Console.ReadLine();
+            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(altura);
+
+            Console.ReadLine();
 
         }
 
